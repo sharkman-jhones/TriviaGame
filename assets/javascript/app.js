b9820e4840d825199946c60792d662d33b0143e4
sharkman-jhones <sharkman.jhones@gmail.com>
2016-08-24 14:57:06 -0500

    Corrected links to JS and CSS files, made test object

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var triviaEntry = [
+	{
+		question:"Which famous American musician was fatally shot by his father on April 1, 1984?",
+		answer: "Marvin Gaye",
+		wrong1: "Elvis Presley",
+		wrong2: "Otis Redding",
+		wrong3: "Janis Joplin"},
+	{
+
+	}
+];
+
+$('body').html(console.log(triviaEntry[0]));
+
+$('body').html(console.log(triviaEntry[0].question));
+$('body').html(console.log(triviaEntry[0].answer));
